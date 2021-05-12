6b803519c349e8ad61ed347248392abb35746233
UnDiaDeFuria <pabloar24.7@gmail.com>
2021-05-12 02:01:17 -0300

    comienza proyecto

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express') // trae el modulo de express para poder montar el servidor
+const path = require('path') //requiere el modulo nativo path de node
+const app = express()
+publicPath = path.join(__dirname, 'public') // indica que la ruta contiene recursos estaticos para consumir de manera sencilla
+console.log(publicPath)
+
+app.use(express.static(publicPath));
+
+app.listen(3001, () => console.log('Servidor corriendo en el puerto 3001'))
+
+app.get('/', (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
+app.get('/register', (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+})
+app.get('/login', (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+})
