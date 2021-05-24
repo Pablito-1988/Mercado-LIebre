ef6d76f78d9f7981e3e2f5c823e24c9e96f05e6e
UnDiaDeFuria <pabloar24.7@gmail.com>
2021-05-24 13:48:11 -0300

    h

--- app.js
+++ app.js
@@ -2,23 +2,13 @@ const express = require('express') // trae el modulo de express para poder monta
 const path = require('path') //requiere el modulo nativo path de node
 const app = express()
 const port = process.env.PORT || 3001
+const rutaMain= require('./src/routes/main')
 publicPath = path.join(__dirname, 'public') // indica que la ruta contiene recursos estaticos para consumir de manera sencilla
 console.log(publicPath)
-
 app.use(express.static(publicPath));
 app.set('view engine', 'ejs')
-
 app.listen(port, () => console.log('Servidor corriendo en el puerto 3001'))
 
-app.get('/', (req, res) => {
+app.use('/',rutaMain);
 
-    res.render('home')
-})
-app.get('/register', (req, res) => {
 
-    res.render('register')
-})
-app.get('/login', (req, res) => {
-
-    res.render('login')
-})
