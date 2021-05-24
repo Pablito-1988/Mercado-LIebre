e2b650227441499593c928a294847d1a0d18d5b4
UnDiaDeFuria <pabloar24.7@gmail.com>
2021-05-24 14:05:56 -0300

    ruteado y acomodado

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ const express = require('express') // trae el modulo de express para poder monta
 const path = require('path') //requiere el modulo nativo path de node
 const app = express()
 const port = process.env.PORT || 3001
-const rutaMain= require('./src/routes/main')
-publicPath = path.join(__dirname, 'public') // indica que la ruta contiene recursos estaticos para consumir de manera sencilla
+const rutaMain= require('./routes/main')
+publicPath = path.join(__dirname, '../public') // indica que la ruta contiene recursos estaticos para consumir de manera sencilla
 console.log(publicPath)
 app.use(express.static(publicPath));
 app.set('view engine', 'ejs')
@@ -12,3 +12,5 @@ app.listen(port, () => console.log('Servidor corriendo en el puerto 3001'))
 app.use('/',rutaMain);
 
 
+
+ 
